19305a066a05cebe57774da52390f794cf8f8b9d
Connor Todd <todd37@purdue.edu>
2019-01-13 13:51:59 -0500

    Initialize backend

---
+++ source/addAllPros.sql
@@ -0,0 +1,18 @@
+use listOfPros
+create table ConnorTodd (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table ColleenOleary (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table MattDecker (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table AlyssaLopez (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table BlakeFoster (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table BrockFoster (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table DonCampbell (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table NaveedIqball (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table NickOrourke (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table EricJohnson (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table ClareNorth (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table JimSayyed (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table DrewJackson (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table MarkMees (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table FredSmith (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table LaurenKline (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
+create table MikeWest (date varchar(20), class varchar(50), clinic varchar(50), hours float, privateLessonName varchar(50), privateLessonLength float, chitNumber int);
---
+++ source/deleteAllPros.sql
@@ -0,0 +1,18 @@
+use listOfPros
+drop table ConnorTodd;
+drop table ColleenOleary;
+drop table MattDecker;
+drop table AlyssaLopez;
+drop table DrewJackson;
+drop table NaveedIqball;
+drop table NickOrourke;
+drop table BlakeFoster;
+drop table EricJohnson;
+drop table FredSmith;
+drop table DonCampbell;
+drop table ClareNorth;
+drop table MarkMees;
+drop table JimSayyed;
+drop table LaurenKline;
+drop table BrockFoster;
+drop table MikeWest
