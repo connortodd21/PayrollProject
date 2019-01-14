8518f6c46993da114ec516547ab5d1664596a484
Connor Todd <todd37@purdue.edu>
2019-01-13 22:32:44 -0500

    Edit wording

--- web/backend/middleware/encrypt.js
+++ web/backend/middleware/encrypt.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 
 var encrypt = function (password) {
     return new Promise(function (resolve, reject) {
-        bcrypt.genSalt(3, (err, salt) => {
+        bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(password.toString(), salt, (err, hash) => {
                 if (hash) {
                     resolve(hash)
