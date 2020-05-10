6e59f08bde966210a952b2442eeea47755836d40
Connor Todd <todd37@purdue.edu>
2020-05-10 16:54:00 -0400

    Commit to push changes before factory reset

--- web/frontend/payroll/src/app/app-routing.module.ts
+++ web/frontend/payroll/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //404 error, leave this one as last route check
+    path: '**',
+    redirectTo: 'home',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
